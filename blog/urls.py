536bade00d09568487d1c433f57193d8cfc47e8c
petercodes <ojoiyanuoluwa1999@gmail.com>
2025-11-11 16:03:17 +0100

    created the templates and static files

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('',views.post_list,name="blog_list"),
-    path('detail/', views.post_detail, name='post_detail')
+    path('<int:id>', views.post_detail, name='post_detail')
 ]
